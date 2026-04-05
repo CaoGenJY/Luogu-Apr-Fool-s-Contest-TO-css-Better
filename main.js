a4b2fa92a4a1d81596fa692da4c43e164acd8dd6
CaoGenJY <15721435213@163.com>
2026-04-05 15:12:55 +0800

    Add user script for Luogu Apr Fool's Contest

---
+++ main.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Luogu Apr Fool's Contest TO css Better
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  通过本地路径桥接调试
+// @author       CaoGenJY
+// @match        https://www.luogu.com.cn/record/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+    console.log('main.js已启动');
+    // 1. 获取所有 class 为 test-case 的 div
+
+    const timer = setTimeout(() => {
+        const testCases = document.querySelectorAll('.test-case');
+        const infos = document.querySelectorAll('.test-case .content .info');
+        const statuses = document.querySelectorAll('.test-case .content .status');
+        const messages = document. querySelectorAll('.wrapper .message');
+        const length = testCases.length;
+        for (var i = 0; i < length; i ++ ) {
+            var div = testCases[i], info = infos[i], status = statuses[i], message = messages[i]
+            var inf = info.innerText.split('ms/');
+            info.innerText = `第${inf[0]}关`;
+            if (status.innerText == 'AC') {
+                status.innerText = '玩家';
+                info.innerText = '';
+                message.innerText = '玩家';
+            }
+            if (status.innerText == 'MLE') {
+                if (inf[1] == '0B') {
+                    div.style.setProperty('background', 'rgb(104, 2, 248)')
+                    status.innerText = '主传送门';
+                    message.innerText = '主传送门';
+                } else if (inf[1] == '1.00MB') {
+                    div.style.setProperty('background', 'rgb(159, 93, 252)')
+                    status.innerText = '传送门';
+                    message.innerText = '传送门';
+                } else if(inf[1] == '999.00MB') {
+                    status.innerText = '墙';
+                    message.innerText = '墙';
+                }
+            }
+            if (status.innerText == 'WA') {
+                div.style.setProperty('background', 'rgb(138, 138, 138)');
+                status.innerText = '空地';
+                info.innerText = '';
+                message.innerText = '空地';
+            }
+        }
+        clearTimeout(timer);
+    }, 500);
+})();
